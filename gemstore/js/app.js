e161029fbd0b60f31dc96c8c255983fa4f7dbe20
Ivo Stoyanov <ivostoy@yahoo.com>
2014-10-25 20:05:03 -0700

    directives

--- gemstore/js/app.js
+++ gemstore/js/app.js
@@ -1,9 +1,6 @@
-/**
- * 
- */
-
 (function() {
-	var app = angular.module('gemStore', []);
+	var app = angular.module('gemStore', ['store-directives']);
+
 	app.controller('StoreController', function() {
 		this.products = gems;
 	});
@@ -17,50 +14,6 @@
 		};
 	});
 
-	app.directive("productDescription", function() {
-		return {
-			restrict : 'E',
-			templateUrl : 'product-description.html'
-		};
-	});
-
-	app.directive("productSpecs", function() {
-		return {
-			restrict : 'A',
-			templateUrl : "product-specs.html"
-		};
-	});
-
-	app.directive("productGallery", function() {
-		return {
-			restrict : 'E',
-			templateUrl : "product-gallery.html",
-			controller : function() {
-				this.current = 0;
-				this.setCurrent = function(imageNumber) {
-					this.current = imageNumber || 0;
-				};
-			},
-			controllerAs : 'gallery'
-		};
-	});
-
-	app.directive("productTabs", function() {
-		return {
-			restrict : "E",
-			templateUrl : "product-tabs.html",
-			controller : function() {
-				this.tab = 1;
-				this.isSet = function(checkTab) {
-					return this.tab === checkTab;
-				};
-				this.setTab = function(setTab) {
-					this.tab = setTab;
-				};
-			},
-			controllerAs : 'tab'
-		};
-	});
 
 	var gems = [
 			{
