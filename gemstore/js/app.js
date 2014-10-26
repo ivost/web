14d61d74dbf4d74fd2ecf59a878b2232e5d191d9
Ivo Stoyanov <ivostoy@yahoo.com>
2014-10-25 20:59:19 -0700

    CRF

--- gemstore/js/app.js
+++ gemstore/js/app.js
@@ -5,13 +5,13 @@
 		this.products = gems;
 	});
 
-	// app.controller('StoreController', [ '$http', function($http) {
-	// var store = this;
-	// store.products = [];
-	// $http.get('/products.json').success(function(data) {
-	// store.products = data;
-	// });
-	// } ]);
+//	 app.controller('StoreController', [ '$http', function($http) {
+//		var store = this;
+//		store.products = [];
+//		$http.get('products.json').success(function(data) {
+//			store.products = data;
+//		});
+//	} ]);
 
 	app.controller('ReviewController', function() {
 		this.review = {};
