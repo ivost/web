36338d2b058caa0b67c8e7679f0f3a06af758ad3
Ivo Stoyanov <ivostoy@yahoo.com>
2014-10-25 19:50:07 -0700

    directives

--- gemstore/js/app.js
+++ gemstore/js/app.js
@@ -8,22 +8,57 @@
 		this.products = gems;
 	});
 
-	app.controller('TabController', function() {
-		this.tab = 1;
-		this.setTab = function(t) {
-			this.tab = t;
-		};
-		this.isSet = function(t) {
-			return this.tab == t;
-		};
-	});
-
 	app.controller('ReviewController', function() {
 		this.review = {};
 
 		this.addReview = function(product) {
 			product.reviews.push(this.review);
 			this.review = {};
+		};
+	});
+
+	app.directive("productDescription", function() {
+		return {
+			restrict : 'E',
+			templateUrl : 'product-description.html'
+		};
+	});
+
+	app.directive("productSpecs", function() {
+		return {
+			restrict : 'A',
+			templateUrl : "product-specs.html"
+		};
+	});
+
+	app.directive("productGallery", function() {
+		return {
+			restrict : 'E',
+			templateUrl : "product-gallery.html",
+			controller : function() {
+				this.current = 0;
+				this.setCurrent = function(imageNumber) {
+					this.current = imageNumber || 0;
+				};
+			},
+			controllerAs : 'gallery'
+		};
+	});
+
+	app.directive("productTabs", function() {
+		return {
+			restrict : "E",
+			templateUrl : "product-tabs.html",
+			controller : function() {
+				this.tab = 1;
+				this.isSet = function(checkTab) {
+					return this.tab === checkTab;
+				};
+				this.setTab = function(setTab) {
+					this.tab = setTab;
+				};
+			},
+			controllerAs : 'tab'
 		};
 	});
 
