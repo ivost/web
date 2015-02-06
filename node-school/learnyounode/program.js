54a7743f0e8413cd3da9b1aa09b4c859aaeeed65
Ivo Stoyanov <ivostoy@yahoo.com>
2015-02-05 19:51:01 -0800

    nodeschool

---
+++ node-school/learnyounode/program.js
@@ -0,0 +1,19 @@
+/* STEP 2
+var result = 0
+
+    for (var i = 2; i < process.argv.length; i++)
+      result += Number(process.argv[i])
+
+    console.log(result)
+*/
+
+/*
+console.log("HELLO WORLD");
+//console.log(process.argv);
+var a = process.argv.slice(2)
+var sum = 0
+for (idx=0; idx<a.length; idx++) {
+    sum += Number(a[idx])
+}
+console.log(sum)
+*/
