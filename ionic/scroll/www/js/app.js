aeceeae4a0503106fc9a05e9f949c25389eff1aa
Ivo Stoyanov <ivostoy@yahoo.com>
2015-08-16 17:31:59 -0700

    canvas

--- ionic/scroll/www/js/app.js
+++ ionic/scroll/www/js/app.js
@@ -15,5 +15,32 @@ angular.module('starter', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+    var canvas = document.getElementById("c1");
+    var img = document.getElementById("p1");
+    var context = canvas.getContext("2d");
+
+    var pattern = context.createPattern(img, 'repeat');
+    context.rect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = pattern;
+    context.fill();
+
+    var canvas2 = document.getElementById("c2");
+    var context2 = canvas2.getContext("2d");
+
+    var pattern2 = context2.createPattern(img, 'repeat');
+    context2.rect(0, 0, canvas2.width, canvas2.height);
+    context2.fillStyle = pattern2;
+    context2.fill();
+
+    // // Create gradient
+    // var grd = ctx.createRadialGradient(2500, 500, 200, 2500, 500, 3000);
+    // grd.addColorStop(0,"red");
+    // grd.addColorStop(1,"white");
+
+    // Fill with gradient
+    // ctx.fillStyle = grd;
+    // ctx.fillRect(0, 0, 5000,100);
+
   });
 })
