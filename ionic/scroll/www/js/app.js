e6846fbd078d78bf794733cbdb1d792ff93fcd14
Ivo Stoyanov <ivostoy@yahoo.com>
2015-08-27 00:39:28 -0700

    scroll

--- ionic/scroll/www/js/app.js
+++ ionic/scroll/www/js/app.js
@@ -1,9 +1,64 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('todo', ['ionic'])
+.controller('MainCtrl', function($scope, $ionicScrollDelegate) {
+  //console.log('MAIN CTRL');
+  var v1 = $ionicScrollDelegate.$getByHandle('A1');
+  var v2 = $ionicScrollDelegate.$getByHandle('A2');
+  var x1 = 0;
+  var x2 = 0;
+  var px1 = 1;
+  var px2 = 1;
+
+  $scope.scrolledA1 = function() {
+    //console.log(v1);
+    x1 = v1.getScrollPosition().left;
+    if (Math.abs(x1 - px1) >= 1) {
+      px1 = x1;
+      //console.log("A1 x1 " + x1);
+      v2.scrollTo(x1, 0, 1);
+    }
+  };
+
+  $scope.scrolledA2 = function() {
+    x2 = v2.getScrollPosition().left;
+    if (Math.abs(x2 - px2) >= 1) {
+      //console.log("A2 x2 " + x2);
+      px2 = x2;
+      v1.scrollTo(x2, 0, 1);
+    }
+
+    // if (!f2) {
+    //   console.log('scrolledA2');
+    //   f2 = true;
+    //   v1.scrollTo(v2.getScrollPosition());
+    //   f2 = false;
+    // }
+    //console.log(v1.getScrollPosition());
+    //v1.scrollTo(v2.getScrollPosition());
+  };
+
+});
+
+/*
+//console.log(v2.getScrollPosition());
+// if (!f1) {
+//   console.log('scrolledA1');
+//   f1 = true;
+//   v2.scrollTo(v1.getScrollPosition());
+//   f1 = false;
+// }
+
+$scope.scrollA1 = function() {
+  console.log('scrollA1');
+  console.log(v1.getScrollPosition());
+  console.log(v2.getScrollPosition());
+};
+
+$scope.scrollA2 = function() {
+  console.log('scrollA2');
+  console.log(v1.getScrollPosition());
+  console.log(v2.getScrollPosition());
+};
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -15,7 +70,8 @@ angular.module('starter', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
-
+*/
+/*
     var canvas = document.getElementById("c1");
     var img = document.getElementById("p1");
     var context = canvas.getContext("2d");
@@ -32,7 +88,7 @@ angular.module('starter', ['ionic'])
     context2.rect(0, 0, canvas2.width, canvas2.height);
     context2.fillStyle = pattern2;
     context2.fill();
-
+*/
     // // Create gradient
     // var grd = ctx.createRadialGradient(2500, 500, 200, 2500, 500, 3000);
     // grd.addColorStop(0,"red");
@@ -41,6 +97,5 @@ angular.module('starter', ['ionic'])
     // Fill with gradient
     // ctx.fillStyle = grd;
     // ctx.fillRect(0, 0, 5000,100);
-
-  });
-})
+//   });
+// })
