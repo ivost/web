f3d99afbfa123d90b6c759ed00da6d22278157c4
Ivo Stoyanov <ivostoy@gmail.com>
2016-04-17 10:24:17 -0700

    port

--- SpringJerseyDemo/src/test/java/com/example/StatusServiceIntegrationTest.java
+++ SpringJerseyDemo/src/test/java/com/example/StatusServiceIntegrationTest.java
@@ -3,9 +3,13 @@ package com.example;
 import com.example.domain.Status;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -16,16 +20,25 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
-@IntegrationTest("server.port=8088")
+@IntegrationTest("server.port=0")
+//@WebIntegrationTest("server.port:0")
 @WebAppConfiguration
 public class StatusServiceIntegrationTest {
+
+	@Autowired
+	EmbeddedWebApplicationContext server;
+
+	@Value("${local.server.port}")
+	int port;
 
 	private RestTemplate restTemplate = new TestRestTemplate();
 
 	@Test
 	public void statusShouldBeOK() {
+		String url = "http://localhost:" + port + "/status";
+		System.out.println("Status test - port: " + port);
 		ResponseEntity<Status> entity =
-				restTemplate.getForEntity("http://localhost:8088/status", Status.class);
+				restTemplate.getForEntity(url, Status.class);
         Status status = entity.getBody();
 		assertThat("HTTP OK", entity.getStatusCode().is2xxSuccessful());
 		assertThat("status 1", status.getCode(), is(1));
