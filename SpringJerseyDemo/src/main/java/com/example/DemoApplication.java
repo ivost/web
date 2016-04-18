d29e8432626ecdccb6d6e7ecca8396d12f240360
Ivo Stoyanov <ivostoy@gmail.com>
2016-04-17 17:13:22 -0700

    wip

--- SpringJerseyDemo/src/main/java/com/example/DemoApplication.java
+++ SpringJerseyDemo/src/main/java/com/example/DemoApplication.java
@@ -7,27 +7,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
  http://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#build-tool-plugins-maven-packaging
 
-https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-external-config.html
+ https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-external-config.html
 
-spring.main.web_environment=false
-spring.main.banner_mode=off
+Actuator HTTP endpoints are only available for Spring MVC-based applications.
+If you want to use Jersey and still use the actuator you will need to enable
+Spring MVC(by depending on spring-boot-starter-web,for example).
+By default,both Jersey and the Spring MVC dispatcher servlet are mapped
+to the same path(/).You will need to change the path for one of them
+(by configuring server.servlet-path for Spring MVC or
+spring.jersey.application-path for Jersey).
+For example, if you add server.servlet-path=/system into application.properties,
+the actuator HTTP endpoints will be available under/system.
 
-environments:
-    dev:
-        name: Developer Setup
-        url: http://dev.bar.com
-    default:
-        name: My Cool App
-        url: http://foo.bar.com
 
  */
+
 
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(DemoApplication.class, args);
-	}
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
