ae89cb83bf637165720dc2b32fbf35c107b1a4f6
Ivo Stoyanov <ivostoy@gmail.com>
2018-02-23 01:04:11 -0800

    wip

--- video/main.js
+++ video/main.js
@@ -1,27 +1,30 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-// var textarea = document.getElementById("code");
-// var reset = document.getElementById("reset");
-// var edit = document.getElementById("edit");
-// var code = textarea.value;
+document.addEventListener('DOMContentLoaded', function(){
+    var v = document.getElementById('v');
+    var canvas = document.getElementById('c');
+    var ctx = canvas.getContext('2d');
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "pink";
-  ctx.fillRect(10, 10, 100, 100);
-  //eval(textarea.value);
+    // var cw = Math.floor(canvas.clientWidth / 100);
+    // var ch = Math.floor(canvas.clientHeight / 100);
+    // canvas.width = cw;
+    // canvas.height = ch;
+
+    var cw = Math.floor(canvas.clientWidth / 10);
+    var ch = Math.floor(canvas.clientHeight / 10);
+    console.log("cw", cw, "ch", ch);
+    v.addEventListener('play', function(){
+        draw(this, ctx, cw, ch);
+    },false);
+
+},false);
+
+function draw(v, c, w, h) {
+    if (v.paused || v.ended) {
+    	return false;
+    }
+	c.fillStyle = "pink";
+	c.fillRect(10, 10, 100, 100);
+    setTimeout(draw, 20, v, c, w, h);
 }
-/*
-reset.addEventListener("click", function() {
-  textarea.value = code;
-  drawCanvas();
-});
 
-edit.addEventListener("click", function() {
-  textarea.focus();
-})
-textarea.addEventListener("input", draw);
-*/
 
-window.addEventListener("load", draw);
 
