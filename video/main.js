cbbfba680f90f48117a88f2a861f6b8c2f78af6a
Ivo Stoyanov <ivostoy@gmail.com>
2018-02-24 14:46:10 -0800

    wip

--- video/main.js
+++ video/main.js
@@ -6,15 +6,11 @@ document.addEventListener('DOMContentLoaded', function(){
     var ctx = canvas.getContext('2d');
     var x = 0
     var y = 0
-    // rectangle dim
-    var cw = 20;
-    var ch = 100;
 
     console.log(v.textTracks)
-    v.addEventListener('play', function(){
-      //  draw(this, ctx, x, y, cw, ch);
-    }, false);
+
     var trackElements = document.querySelectorAll("track");
+    //var prev_coord
     trackElements[0].addEventListener('load', function() {
       var textTrack = this.track;
       textTrack.oncuechange = function() {
@@ -22,47 +18,30 @@ document.addEventListener('DOMContentLoaded', function(){
         if(!cue) {
           return;
         }
-        var coords = JSON.parse(cue.text);
-        console.log('cue updated', coords)
-        draw_triangle(v, ctx, coords.x1, coords.y1, coords.x2, coords.y2)
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // if (prev_coord) {
+        //   coord = prev_coord
+        // }
+        var coord = JSON.parse(cue.text);
+        console.log('cue updated', coord)
+        draw_triangle(v, ctx, coord.a, coord.b, coord.c)
+        // prev_coord = coord
       }
     })
 
 }, false);
 
-function draw(v, c, x, y, w, h) {
-    //console.log(v.textTracks[0].activeCues)
-    if (v.paused || v.ended) {
-    	return false;
-    }
-    //c.clearRect(x, y, w, h);
-    x = x + 1
-    y = y + 1
-    if (x > 100) {
-        x = 1
-        y = 1
-    }
-    //console.log(x, y)
-    //c.fillStyle = 'rgba(100, 0, 0, 0.5)';
-    c.fillStyle = 'yellow';
-    c.fillRect(x, y, w, h);
-    //setTimeout(draw, 40, v, c, x, y, w, h);
-}
 
 // draw upward pointing triangle
-function draw_triangle(v, ctx, x, y, halfSide, rowHeight) {
+function draw_triangle(v, ctx, a, b, c) {
     ctx.fillStyle = 'yellow';
     ctx.strokeStyle = 'yellow';
     ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x + halfSide, y + rowHeight);
-    ctx.lineTo(x - halfSide, y + rowHeight);
+    ctx.moveTo(a[0], a[1]);
+    ctx.lineTo(b[0], b[1]);
+    ctx.lineTo(c[0], c[1]);
     ctx.closePath();
     ctx.fill();
     ctx.stroke(); // needed to fill antialiased gaps on edges
-
 }
 
-//      context.translate(canvas.width / 2, canvas.height / 2);
-// context.globalAlpha = 0.5;
-// context.beginPath();
