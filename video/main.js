0660e7f16c4d30b6f0819cd805bc399c7df3d797
Ivo Stoyanov <ivostoy@gmail.com>
2018-02-24 14:25:29 -0800

    victor

--- video/main.js
+++ video/main.js
@@ -10,17 +10,32 @@ document.addEventListener('DOMContentLoaded', function(){
     var cw = 20;
     var ch = 100;
 
+    console.log(v.textTracks)
     v.addEventListener('play', function(){
-        draw(this, ctx, x, y, cw, ch);
+      //  draw(this, ctx, x, y, cw, ch);
     }, false);
+    var trackElements = document.querySelectorAll("track");
+    trackElements[0].addEventListener('load', function() {
+      var textTrack = this.track;
+      textTrack.oncuechange = function() {
+        var cue = this.activeCues[0];
+        if(!cue) {
+          return;
+        }
+        var coords = JSON.parse(cue.text);
+        console.log('cue updated', coords)
+        draw_triangle(v, ctx, coords.x1, coords.y1, coords.x2, coords.y2)
+      }
+    })
 
 }, false);
 
 function draw(v, c, x, y, w, h) {
+    //console.log(v.textTracks[0].activeCues)
     if (v.paused || v.ended) {
     	return false;
     }
-    c.clearRect(x, y, w, h);
+    //c.clearRect(x, y, w, h);
     x = x + 1
     y = y + 1
     if (x > 100) {
@@ -31,16 +46,13 @@ function draw(v, c, x, y, w, h) {
     //c.fillStyle = 'rgba(100, 0, 0, 0.5)';
     c.fillStyle = 'yellow';
     c.fillRect(x, y, w, h);
-    setTimeout(draw, 40, v, c, x, y, w, h);
+    //setTimeout(draw, 40, v, c, x, y, w, h);
 }
 
 // draw upward pointing triangle
-function draw_triangle() {
-    var clr;
-
-    clr = 'hsl(' + hue + ', 50%, ' + rnd(0, 60) + '%)';
-    ctx.fillStyle = clr;
-    ctx.strokeStyle = clr;
+function draw_triangle(v, ctx, x, y, halfSide, rowHeight) {
+    ctx.fillStyle = 'yellow';
+    ctx.strokeStyle = 'yellow';
     ctx.beginPath();
     ctx.moveTo(x, y);
     ctx.lineTo(x + halfSide, y + rowHeight);
