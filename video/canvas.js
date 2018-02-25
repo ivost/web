9f5be6a674133e5b0fafd6f4141ca83fa900f63d
Ivo Stoyanov <ivostoy@gmail.com>
2018-02-24 16:58:12 -0800

    wip

--- video/canvas.js
+++ video/canvas.js
@@ -3,31 +3,34 @@
 document.addEventListener('DOMContentLoaded', function(){
     var canvas = document.getElementById('c');
     var ctx = canvas.getContext('2d');
-    var x = 0
-    var y = 0
-    // rectangle dim
-    var cw = 20;
-    var ch = 100;
-    // draw rectangle here
-    draw_triangle(ctx)
+    var cw = canvas.width
+    var ch = canvas.height
+    var midx = Math.floor(cw/2)
+
+    ctx.fillStyle = 'yellow';
+    ctx.strokeStyle = 'yellow';
+    ctx.lineWidth = 3;
+    ctx.strokeRect(0, 0, cw, ch);
+
+    var a = [midx,  20];
+    var b = [midx-100, 600];
+    var c = [midx+100, 600];
+    draw_triangle(ctx, a, b, c)
 }, false);
 
 
-// draw upward pointing triangle
-function draw_triangle(ctx) {
-    var clr;
-
-    clr = 'hsl(' + hue + ', 50%, ' + rnd(0, 60) + '%)';
-    ctx.fillStyle = clr;
-    ctx.strokeStyle = clr;
+// draw triangle defined by 3 points a, b, c
+function draw_triangle(ctx, a, b, c) {
+    ctx.fillStyle = 'yellow';
+    ctx.strokeStyle = 'yellow';
+    console.log(a, b, c)
     ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x + halfSide, y + rowHeight);
-    ctx.lineTo(x - halfSide, y + rowHeight);
+    ctx.moveTo(a[0], a[1]);
+    ctx.lineTo(b[0], b[1]);
+    ctx.lineTo(c[0], c[1]);
     ctx.closePath();
     ctx.fill();
     ctx.stroke(); // needed to fill antialiased gaps on edges
-
 }
 
 // context.translate(canvas.width / 2, canvas.height / 2);
--- video/main.js
+++ video/main.js
@@ -4,13 +4,12 @@ document.addEventListener('DOMContentLoaded', function(){
     var v = document.getElementById('v');
     var canvas = document.getElementById('c');
     var ctx = canvas.getContext('2d');
-    var x = 0
-    var y = 0
+    var cw = canvas.width
+    var ch = canvas.height
+    var midx = Math.floor(cw/2)
 
     console.log(v.textTracks)
-
     var trackElements = document.querySelectorAll("track");
-    //var prev_coord
     trackElements[0].addEventListener('load', function() {
       var textTrack = this.track;
       textTrack.oncuechange = function() {
@@ -18,30 +17,34 @@ document.addEventListener('DOMContentLoaded', function(){
         if(!cue) {
           return;
         }
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        // if (prev_coord) {
-        //   coord = prev_coord
-        // }
+        console.log(cue)
         var coord = JSON.parse(cue.text);
-        console.log('cue updated', coord)
-        draw_triangle(v, ctx, coord.a, coord.b, coord.c)
-        // prev_coord = coord
+        //console.log('coord', coord)
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // for debug
+        // ctx.fillStyle = 'yellow';
+        // ctx.strokeStyle = 'yellow';
+        // ctx.lineWidth = 3;
+        // ctx.strokeRect(0, 0, cw, ch);
+
+        draw_triangle(ctx, coord.a, coord.b, coord.c)
       }
     })
 
 }, false);
 
-
-// draw upward pointing triangle
-function draw_triangle(v, ctx, a, b, c) {
-    ctx.fillStyle = 'yellow';
-    ctx.strokeStyle = 'yellow';
+// draw triangle defined by 3 points a, b, c
+function draw_triangle(ctx, a, b, c) {
+    console.log(a, b, c)
+    ctx.fillStyle = 'green';
+    ctx.strokeStyle = 'green';
     ctx.beginPath();
     ctx.moveTo(a[0], a[1]);
     ctx.lineTo(b[0], b[1]);
     ctx.lineTo(c[0], c[1]);
     ctx.closePath();
     ctx.fill();
-    ctx.stroke(); // needed to fill antialiased gaps on edges
+    // fill antialiased gaps on edges
+    ctx.stroke(); 
 }
 
