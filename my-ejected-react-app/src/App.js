4ae36bdf659ef50fbfaf90a680ea9de62154d029
Ivostoysd <ivo.stoyanov@smartdrive.net>
2018-05-12 12:39:58 -0700

    test

--- my-ejected-react-app/src/App.js
+++ my-ejected-react-app/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import logo from './logo.svg'; 
 import './App.css';
 import Heading from './Heading';
 import Button from './Button';
+import Repeat from './Repeat'; 
 
 class App extends Component {
   render() {
@@ -11,6 +13,10 @@ class App extends Component {
           My App Heading
         </Heading>
         <Button />
+
+        <p className="App-intro"> 
+          <Repeat times="5" value="React!" /> 
+        </p> 
       </div>
     );
   }
---
+++ my-ejected-react-app/src/Repeat.js
@@ -0,0 +1,5 @@
+export default ({ times, value }) => 
+  new Array(parseInt(times, 10))
+    .fill(value)
+    .join(' ');
+    
